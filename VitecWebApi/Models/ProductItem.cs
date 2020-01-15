3eb1169e96a411b750e51451cf19a84ba3343d4c
Søren Eriksen <soer33b0@edu.eal.dk>
2020-01-15 01:24:53 +0100

    DataAnnotations

--- VitecWebApi/Models/ProductItem.cs
+++ VitecWebApi/Models/ProductItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,18 @@ namespace VitecWebApi.Models
     public class ProductItem
     {
         public int Id { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Enter title")]
+        [StringLength(30)]
         public string Title { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string ProductDescription { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
         public float Price { get; set; }
     }
 }
